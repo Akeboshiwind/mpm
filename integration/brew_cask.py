b300e35b72219d4f7d2ab92c14f5a0f4c2e596ff
Oliver Marshall <olivershawmarshall@gmail.com>
2019-09-18 18:20:15 +0100

    Assume if a list is not input then it's a single package

--- integration/brew_cask.py
+++ integration/brew_cask.py
@@ -17,18 +17,24 @@ class BrewCask(core.PackageManager):
         return pkgs
 
     def install(self, pkgs):
+        if type(pkgs) is not list:
+            pkgs = [pkgs]
         out = subprocess.call("brew cask install".split() + pkgs,
                               stdout=subprocess.DEVNULL,
                               stderr=subprocess.DEVNULL)
         return out == 0
 
     def uninstall(self, pkgs):
+        if type(pkgs) is not list:
+            pkgs = [pkgs]
         out = subprocess.call("brew cask uninstall".split() + pkgs,
                               stdout=subprocess.DEVNULL,
                               stderr=subprocess.DEVNULL)
         return out == 0
 
     def upgrade(self, pkgs=[]):
+        if type(pkgs) is not list:
+            pkgs = [pkgs]
         out = subprocess.call("brew cask upgrade".split() + pkgs,
                               stdout=subprocess.DEVNULL,
                               stderr=subprocess.DEVNULL)
