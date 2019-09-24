f2af4cd99d12e5c35dd36c4d57093312d734322c
Oliver Marshall <olivershawmarshall@gmail.com>
2019-09-24 14:26:49 +0100

    Make the printing in the update command use verbosity

--- command/update.py
+++ command/update.py
@@ -23,37 +23,47 @@ def command(args, cfg):
 
         # Update the manager
         print("[" + manager.config_name + "] update")
+
+        if args.verbose >= 1 or args.dry_run:
+            print(manager.config_name + " updated")
+
         if not(args.dry_run):
             manager.update()
-        else:
-            print(manager.config_name + " would have been updated")
 
         # Install new packages
         print("[" + manager.config_name + "] install new packages")
+
         # Compare with the list of full packages because some of the requested
         # packages might be dependencies on others, so .leaves() would be wrong
         new_pkgs = pkgs.difference(manager.list())
+
+        if args.verbose >= 1 or args.dry_run:
+            print("The following " + manager.config_name + " packages will be installed")
+            print(list(new_pkgs))
+
         if not(args.dry_run):
             manager.install(list(new_pkgs))
-        else:
-            print("The following " + manager.config_name + " packages would have been installed")
-            print(list(new_pkgs))
 
         # Uninstall old packages
         print("[" + manager.config_name + "] uninstall old packages")
+
         # Compare with the packages that aren't dependencies (leaves) so that we
         # don't uninstall a required package
         old_pkgs = manager.leaves().difference(pkgs)
+
+        if args.verbose >= 1 or args.dry_run:
+            print("The following " + manager.config_name + " packages will be uninstalled")
+            print(list(old_pkgs))
+
         if not(args.dry_run):
             manager.uninstall(list(old_pkgs))
-        else:
-            print("The following " + manager.config_name + " packages would have been uninstalled")
-            print(list(old_pkgs))
 
         # Update installed packages
         print("[" + manager.config_name + "] update packages")
+
+        if args.verbose >= 1 or args.dry_run:
+            print("The following " + manager.config_name + " packages will be upgraded")
+            print("TODO")
+
         if not(args.dry_run):
             manager.upgrade(list(pkgs))
-        else:
-            print("The following " + manager.config_name + " packages would have been upgraded")
-            print("TODO")
