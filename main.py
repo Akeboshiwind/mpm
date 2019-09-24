7dd456809996b1643cb0b18ef6b0de474359c1dd
Oliver Marshall <olivershawmarshall@gmail.com>
2019-09-24 14:24:37 +0100

    Re-format parser arguments

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ if __name__ == "__main__":
     ## Top level parser
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--version', action='version', version='%(prog)s '+VERSION)
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s '+VERSION)
 
     ## Subcommands
     subparsers = parser.add_subparsers()
 
     ### Update subcommand
-    parser_update = subparsers.add_parser("update", help="update packages (default, if non provided)")
-    parser_update.add_argument("--dry-run", help="Just list the packages, don't actually run any commands", action="store_true")
+    parser_update = subparsers.add_parser("update",
+                                          help="update packages (default, if non provided)")
+    parser_update.add_argument("--dry-run",
+                               help="Just list the packages, don't actually run any commands",
+                               action="store_true")
     parser_update.set_defaults(func=update.command)
 
 
